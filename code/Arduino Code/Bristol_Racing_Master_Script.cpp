95e9b810cf1e3ae6db5321b9ed840017d3bdc0a7
matt-appleby <matthewappleby5965@gmail.com>
2022-09-08 20:23:41 +0100

    thermistor code embedded

--- code/Arduino Code/Bristol_Racing_Master_Script.cpp
+++ code/Arduino Code/Bristol_Racing_Master_Script.cpp
@@ -8,6 +8,8 @@
 // A to 5V via a 10kohm resistor
 // K to GND
 // pin 7 to be used to detect time start
+// Thermistor potential divider wiring:
+// V_in ----R_set---- A0 ----Thermistor---- GND
 
 #include <LiquidCrystal.h>
 
@@ -23,6 +25,14 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int nMinutes = 0;
 int nSeconds = 0;
+
+//thermistor
+const int R_set = 1000.0;// R_set is potential divider resistor,
+const int R_0 = 10000.0;//  R_0 is thermistor resistance at T_0
+const int V_in = 5.0;
+const long int T_0 = 298.0; // T_0 is temperature in Kelvin that the thermistor resistance is known,
+const long int B = 3950.0;//B is a thermistor constant 
+
 
 void setup() {
   pinMode(PWM_pin, OUTPUT);
@@ -48,24 +58,8 @@ void loop() {
 
     analogWrite(PWM_pin, PWM_pct);//PWM out
 
-    
-    //output to lcd
-    // battery - current - motor temp - race time
-    // want: each batteries voltage and speed up or slow down
-
-    // battery [PWM for now]
-    lcd.setCursor(1, 0);
-    lcd.print(PWM_pct);
-
-    // current draw 
-    lcd.setCursor(10, 0);
-    lcd.print("12.3A");
-    
-    // motor temp
-    lcd.setCursor(1, 1);
-    lcd.print("45.6C");
-
-    // im deleting race time for now, add later
+    // time
+    // (im deleting race time for now, add later)
     long int t2 = millis();
     nSeconds = ((t2) % 60000) / 1000;
     nMinutes = (t2) / 60000;
@@ -79,4 +73,24 @@ void loop() {
     lcd.print("0");
     }
     lcd.print(nSeconds);
+
+    // thermistor code
+    int V_value = analogRead(A0);
+    float V_out = V_value * (V_in / 1023.0);
+    float R_thermistor = (V_out * R_set) / (V_in - V_out);
+    float T = ((B * T_0) / (T_0 * log(R_thermistor / R_0) + B)) - 273;//Motor temp
+    
+    //output to lcd
+    // battery - current - motor temp - race time
+    // want: each batteries voltage and speed up or slow down
+
+    // battery [PWM for now]
+    lcd.setCursor(1, 0);
+    lcd.print(PWM_pct);
+    // current draw 
+    lcd.setCursor(10, 0);
+    lcd.print("12.3A");
+    // motor temp
+    lcd.setCursor(1, 1);
+    lcd.print(T);
 }
