69f52c0454e18c9096ab5da00c9b7a47468d35c5
matt-appleby <matthewappleby5965@gmail.com>
2022-09-08 20:11:55 +0100

    combined throttle and lcd code

--- code/Arduino Code/Bristol_Racing_Master_Script.cpp
+++ code/Arduino Code/Bristol_Racing_Master_Script.cpp
@@ -39,14 +39,44 @@ void setup() {
 }
 
 void loop() {
-  //read the throttle value and convert to a 1-255 value
-  throttle_val = analogRead(throttle_pin);
-  throttle_val_pct = (float)100 * ((float)throttle_val - (float)164) / (float)712;
-  PWM_pct = round((float)255 * (float)throttle_val_pct / (float)100);
-  if (PWM_pct >= 254) PWM_pct = 255;
-  if (PWM_pct <= 1) PWM_pct = 0;
+    //read the throttle value and convert to a 1-255 value
+    throttle_val = analogRead(throttle_pin);
+    throttle_val_pct = (float)100 * ((float)throttle_val - (float)164) / (float)712;
+    PWM_pct = round((float)255 * (float)throttle_val_pct / (float)100);
+    if (PWM_pct >= 254) PWM_pct = 255;
+    if (PWM_pct <= 1) PWM_pct = 0;
 
-  analogWrite(PWM_pin, PWM_pct);//PWM out
+    analogWrite(PWM_pin, PWM_pct);//PWM out
 
     
+    //output to lcd
+    // battery - current - motor temp - race time
+    // want: each batteries voltage and speed up or slow down
+
+    // battery [PWM for now]
+    lcd.setCursor(1, 0);
+    lcd.print(PWM_pct);
+
+    // current draw 
+    lcd.setCursor(10, 0);
+    lcd.print("12.3A");
+    
+    // motor temp
+    lcd.setCursor(1, 1);
+    lcd.print("45.6C");
+
+    // im deleting race time for now, add later
+    long int t2 = millis();
+    nSeconds = ((t2) % 60000) / 1000;
+    nMinutes = (t2) / 60000;
+    lcd.setCursor(10, 1);
+    if (nMinutes <= 9) {
+    lcd.print("0");
+    }
+    lcd.print(nMinutes);
+    lcd.print(":");
+    if (nSeconds <= 9) {
+    lcd.print("0");
+    }
+    lcd.print(nSeconds);
 }
