5bef52fd35a5d165ccac60748d095444b20043a6
matt-appleby <matthewappleby5965@gmail.com>
2022-09-08 18:01:08 +0100

    some arduino snippets

---
+++ code/Arduino Code/SD_Datalogger_test/SD_Datalogger_test.ino
@@ -0,0 +1,57 @@
+/*
+  SD card datalogger
+
+  FOR an UNO & MEGA:
+
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** SCK - pin 13
+ ** SS - pin 10
+ ** If you can't figure out the other two pins then just give up...
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 10;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String dataString = "12345";
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
---
+++ code/Arduino Code/Throttle_PWM/Throttle_PWM.ino
@@ -0,0 +1,22 @@
+int PWM_pin = 3;
+int throttle_pin = A0;
+int PWM_pct = 0;
+int throttle_val = 0;
+float throttle_val_pct = 0;
+
+void setup() {
+  pinMode(PWM_pin, OUTPUT);
+}
+
+void loop() {
+  throttle_val = analogRead(throttle_pin);
+  throttle_val_pct = (float)100 * ((float)throttle_val - (float)164) / (float)712;
+
+  PWM_pct = round((float)255 * (float)throttle_val_pct / (float)100);
+
+  if (PWM_pct >= 254) PWM_pct = 255;
+  if (PWM_pct <= 1) PWM_pct = 0;
+
+  analogWrite(PWM_pin, PWM_pct);
+
+}
