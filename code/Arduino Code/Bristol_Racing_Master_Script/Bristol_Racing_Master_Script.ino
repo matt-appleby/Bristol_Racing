4832c4d35a82c9eea1bad13277214e5391024a3d
matt-appleby <matthewappleby5965@gmail.com>
2022-09-16 10:58:41 +0100

    tachometer code included

--- code/Arduino Code/Bristol_Racing_Master_Script/Bristol_Racing_Master_Script.ino
+++ code/Arduino Code/Bristol_Racing_Master_Script/Bristol_Racing_Master_Script.ino
@@ -13,7 +13,7 @@ A5 - Battery voltage 2
 
 1   -   
 2   -  
-3   -  
+3   -  clock
 4   -   d4      LCD      
 5   -   d5      LCD
 6   -   d6      LCD
@@ -33,6 +33,7 @@ A5 - Battery voltage 2
 // A to 5V via a 10kohm resistor
 // K to GND
 
+
 //V sense Connections:
 //PD HIGH Vout - A4
 //PD LOW Vout - A5
@@ -40,6 +41,22 @@ A5 - Battery voltage 2
 #include <SPI.h>
 #include <SD.h>
 #include <LiquidCrystal.h>
+#include "HX711.h"
+
+//Code pinched from: https://forum.arduino.cc/t/rpm-counter-tachometer-with-hall-sensor/621754/3
+
+#define ClockPin 3 // Must be pin 2 or 3
+
+      // My Encoder has 400 Clock pulses per revolution
+      // note that 150000.0 = (60 seonds * 1000000 microseconds)microseconds in a minute / 400 pulses in 1 revolution)
+      // change the math to get the proper multiplier for RPM for your encoder
+     // yours has 4 pulses in 1 revolution
+      // note that 15000000.0 = (60 seconds * 1000000 microseconds)microseconds in a minute / 4 pulses in 1 revolution
+#define Multiplier 60000000.0// don't forget a decimal place to make this number a floating point number
+//#define DOUT  5
+//#define CLK  4
+
+
 
 // throttle
 int throttle_pin = A0;
@@ -69,11 +86,24 @@ int nSeconds = 0;
 
 //sd
 const int chipSelect = 10;
-
+//Tachometer
+volatile long count = 0;
+volatile int32_t dTime; // Delt in time
+volatile bool DataPinVal;
 //
 int race_time = 0;
 int race_percentage = 0;
 int battery_percentage=99;
+
+
+void onPin2CHANGECallBackFunction(){ 
+    static uint32_t lTime; // Saved Last Time of Last Pulse
+    uint32_t cTime; // Current Time
+    cTime = micros(); // Store the time for RPM Calculations
+// calculate the DeltaT between pulses
+    dTime = cTime - lTime; 
+    lTime = cTime;
+}
 
 void setup() {
     Serial.begin(115200);
@@ -101,6 +131,8 @@ void setup() {
         dataFile.println(header);
         dataFile.close();
     }
+  pinMode(ClockPin, INPUT);  
+  attachInterrupt(digitalPinToInterrupt(ClockPin),onPin2CHANGECallBackFunction,RISING);
 }
 
 void loop() {
@@ -165,6 +197,28 @@ void loop() {
     }
     else {
         lcd.print("---");
+    }
+    float DeltaTime;
+    float SpeedInRPM = 0;
+    float aveSpeed = 0;
+    noInterrupts (); 
+    // Because when the interrupt occurs the EncoderCounter and SpeedInRPM could be interrupted while they 
+    // are being used we need to say hold for a split second while we copy these values down. This doesn't keep the 
+    // interrupt from occurring it just slightly delays it while we maneuver values.
+    // if we don't do this we could be interrupted in the middle of copying a value and the result get a corrupted value.
+    DeltaTime = dTime;
+    interrupts ();
+    
+    //SpeedInRPM = Multiplier / DeltaTime; // Calculate the RPM Switch DeltaT to either positive or negative to represent Forward or reverse RPM
+    // use the speed and counter values for whatever you need to do.
+    static unsigned long SpamTimer;
+    SpeedInRPM = (DeltaTime)? Multiplier / DeltaTime: 0.0; // Calculate the RPM Switch DeltaT to either positive or negative to represent Forward or reverse RPM
+    if ( (unsigned long)(millis() - SpamTimer) >= (100)) {
+        SpamTimer = millis();
+        Serial.print(millis());
+        Serial.print(" ");
+        Serial.print(SpeedInRPM , 3);
+        Serial.print(" RPM ");
     }
     delay(500);
 }
